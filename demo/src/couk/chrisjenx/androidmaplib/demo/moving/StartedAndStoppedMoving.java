d33daec0d9289a04ac01b67ce61c156cef743433
Christopher Jenkins <chris.mark.jenkins@gmail.com>
2012-02-12 01:34:02 +0000

    Added start of demo to show the lib features
    Signed-off-by: Christopher Jenkins <chris.mark.jenkins@gmail.com>

---
+++ demo/src/couk/chrisjenx/androidmaplib/demo/moving/StartedAndStoppedMoving.java
@@ -0,0 +1,56 @@
+package couk.chrisjenx.androidmaplib.demo.moving;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+
+import couk.chrisjenx.androidmaplib.AMLController;
+import couk.chrisjenx.androidmaplib.demo.R;
+import couk.chrisjenx.androidmaplib.interfaces.StoppedMovingEvents;
+
+public class StartedAndStoppedMoving extends MapActivity
+{
+
+	private AMLController aml;
+
+	@Override
+	protected void onCreate(Bundle icicle)
+	{
+		super.onCreate(icicle);
+		setContentView(R.layout.activity_map);
+
+		aml = new AMLController(this, R.id.map_view);
+
+		aml.debug(R.id.text1).setStoppedMovingListener(
+				new StoppedMovingEvents()
+				{
+
+					@Override
+					public void onMapStoppedMoving(GeoPoint mapCentre)
+					{
+						// Toast.makeText(StartedAndStoppedMoving.this,
+						// "Map Stopped Moving", Toast.LENGTH_SHORT).show();
+						aml.debug("Map Stopped Moving");
+					}
+
+					@Override
+					public void onMapStartedMoving()
+					{
+						// Toast.makeText(StartedAndStoppedMoving.this,
+						// "Map Started Moving", Toast.LENGTH_SHORT).show();
+						aml.debug("Map Started Moving");
+					}
+				});
+
+	}
+
+	@Override
+	protected boolean isRouteDisplayed()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
